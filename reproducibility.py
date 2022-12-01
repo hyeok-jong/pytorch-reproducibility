60cfde1c250e06dba5d572f29fc38cd446bc7324
HyeokJong <jj770206@naver.com>
2022-12-01 09:09:28 +0900

    update

---
+++ reproducibility.py
@@ -0,0 +1,11 @@
+def set_random(random_seed):
+    import torch
+    import numpy as np
+    import random
+    torch.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(random_seed)
+    random.seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
